27049bf5064157ba5dcc896fcfdadb83095e2d04
gitzein <zeinfm123@gmai.com>
2024-11-19 23:40:18 +0700

    fix commit err

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,12 +3,15 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
 import { getCurrentUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export const dynamic = "force-dynamic";
 
 async function layout({ children }: { children: ReactNode }) {
   const currentUser = await getCurrentUser();
+
+  if (!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
